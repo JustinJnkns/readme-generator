54b9da3cd3c321b0b678386e1e3edc48a71e7133
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-10-11 20:35:06 -0400

    added readme generation and linked  index.js to genereteMarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,19 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateReadMe = require('./utils/generateMarkdown.js')
+
+
 // array of questions for user
 const questions = [
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+fs.writeFile('./README.md', generateReadMe, err => {
+    if (err) throw new Error(err);
+
+    console.log('Page created! Check out README.md in this directory to see it!');
+});
 
 // function to initialize program
 function init() {
